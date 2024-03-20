5292a6ffc3c4b6f92018c7fdb07151015eaca384
vetkover <vetkover@gmail.com>
2024-03-20 18:36:04 +0100

    chats fixed display of the last hidden message

--- backend/api/chats/list.js
+++ backend/api/chats/list.js
@@ -1,30 +1,31 @@
 const express = require("express");
-const parseCookies  = require('../../components/stuff/cookieReader.js')
+const parseCookies = require('../../components/stuff/cookieReader.js');
 router = express.Router();
 
-const userData = require('../../components/mongoDB/userData.js')
-const chatInfo = require('../../components/mongoDB/chatInfo.js')
+const userData = require('../../components/mongoDB/userData.js');
+const chatInfo = require('../../components/mongoDB/chatInfo.js');
 
 router.get("/list", async (req, res) => {
     const cookie = parseCookies(req.headers.cookie);
-    if(cookie){
+    if (cookie) {
         const DBuserData = await userData(cookie.token);
 
-        const resObj = []
-        if(DBuserData){
+        const resObj = [];
+        if (DBuserData) {
 
-            for(let i = 0; i < DBuserData.chats; i++){
-                let chatDB = await chatInfo(DBuserData.chats[i])
+            for (let i = 0; i < DBuserData.chats.length; i++) {
+                let chatDB = await chatInfo(DBuserData.chats[i]);
+                let lastActiveMessage = chatDB.messages.slice().reverse().find(msg => msg.status === 'active');
+
                 let pushObj = {
                     chatId: chatDB.chatId,
                     chatName: "example",
-                    chatLastMessage: chatDB.messages[chatDB.messages.length-1]?.text ? chatDB.messages[chatDB.messages.length-1].text : "",
+                    chatLastMessage: lastActiveMessage ? lastActiveMessage.text : "",
                     accessUsers: chatDB.accessUsers,
                     type: chatDB.type
-                }
-                resObj.push(pushObj)
+                };
+                resObj.push(pushObj);
             }
-            
 
             res.json(resObj);
         } else {
